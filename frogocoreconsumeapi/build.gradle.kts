b884c83536a1b7bca5da475575961b598964d018
Faisal Amir <faisalamircs@gmail.com>
2022-04-09 07:34:55 +0700

    Update build.gradle.kts

--- frogocoreconsumeapi/build.gradle.kts
+++ frogocoreconsumeapi/build.gradle.kts
@@ -23,27 +23,27 @@ tasks.withType<KotlinCompile> {
 
 dependencies {
 
-    implementation(compose.ui)
-    implementation(compose.runtime)
-    implementation(compose.preview)
-    implementation(compose.uiTooling)
-    implementation(compose.material)
-    implementation(compose.materialIconsExtended)
+    api(compose.ui)
+    api(compose.runtime)
+    api(compose.preview)
+    api(compose.uiTooling)
+    api(compose.material)
+    api(compose.materialIconsExtended)
 
-    implementation(Google.gson)
+    api(Google.gson)
 
-    implementation(Square.OkHttp.okhttp)
-    implementation(Square.OkHttp.loggingInterceptor)
+    api(Square.OkHttp.okhttp)
+    api(Square.OkHttp.loggingInterceptor)
 
-    implementation(Square.Retrofit2.retrofit)
-    implementation(Square.Retrofit2.converterGson)
-    implementation(Square.Retrofit2.adapterRxJava3)
+    api(Square.Retrofit2.retrofit)
+    api(Square.Retrofit2.converterGson)
+    api(Square.Retrofit2.adapterRxJava3)
 
-    implementation(Reactivex.rxJava3)
+    api(Reactivex.rxJava3)
 
-    implementation(Koin.core)
+    api(Koin.core)
 
-    implementation(DependencyGradle.FROGO_SDK_CORE)
+    api(DependencyGradle.FROGO_SDK_CORE)
 
 }
 
