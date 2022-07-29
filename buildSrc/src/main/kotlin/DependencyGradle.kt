f0825d9d6aff4ffb7a91031efef8d6db0354366e
amirisback <faisalamircs@gmail.com>
2022-07-30 00:35:59 +0700

    Release v2.3.9

--- buildSrc/src/main/kotlin/DependencyGradle.kt
+++ buildSrc/src/main/kotlin/DependencyGradle.kt
@@ -16,7 +16,7 @@ object DependencyGradle {
     const val KOTLIN_VERSION = Version.JetBrains.kotlin
     const val COMPOSE_MULTIPLATFORM_VERSION = Version.Androidx.composeMultiPlatform
 
-    const val FROGO_SDK_VERSION = "2.0.6"
+    const val FROGO_SDK_VERSION = "2.0.7"
     const val FROGO_SDK = "com.github.frogobox:frogo-sdk:$FROGO_SDK_VERSION"
     const val FROGO_SDK_CORE = "com.github.frogobox.frogo-sdk:frogocoresdk:$FROGO_SDK_VERSION"
 
@@ -24,7 +24,7 @@ object DependencyGradle {
     const val FROGO_UI = "com.github.frogobox:frogo-ui:$FROGO_UI_VERSION"
     const val FROGO_UI_CORE = "com.github.frogobox.frogo-ui:frogocoreui:$FROGO_UI_VERSION"
 
-    const val FROGO_RECYCLER_VIEW_VERSION = "4.2.2"
+    const val FROGO_RECYCLER_VIEW_VERSION = "4.3.0"
     const val FROGO_RECYCLER_VIEW = "com.github.amirisback:frogo-recycler-view:$FROGO_RECYCLER_VIEW_VERSION"
 
     const val FROGO_PATH_CORE_CONSUME_API = ":frogocoreconsumeapi"
